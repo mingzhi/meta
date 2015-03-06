927792876aee4247a9ba073cdad5185e7ce43f8a
Mingzhi Lin <mingzhi9@gmail.com>
2015-03-06 15:25:54 -0500

    profile: using config

--- cmd/meta/profile.go
+++ cmd/meta/profile.go
@@ -3,22 +3,36 @@ package main
 import (
 	"flag"
 	"github.com/mingzhi/meta"
+	"github.com/spf13/viper"
 	"log"
 	"path/filepath"
+	"runtime"
 )
 
 type cmdProfile struct {
-	workspace *string
-	prefix    *string
-	ref       *string
+	workspace *string // workspace.
+	config    *string // configure file name.
+	prefix    *string // prefix of bacterial species.
+	ncpu      *int    // number of CPUs for using.
+
+	ref string // reference genome database.
 }
 
 func (cmd *cmdProfile) Flags(fs *flag.FlagSet) *flag.FlagSet {
+	cmd.config = fs.String("c", "config", "configure file name")
 	cmd.workspace = fs.String("w", "", "workspace")
-	cmd.ref = fs.String("r", "", "reference genome diretory")
 	cmd.prefix = fs.String("p", "", "prefix")
+	cmd.ncpu = fs.Int("cpu", runtime.NumCPU(), "Number of CPUs for using")
 
 	return fs
+}
+
+func (cmd *cmdProfile) init() {
+	viper.SetConfigName(*cmd.config)
+	viper.AddConfigPath(*cmd.workspace)
+	viper.ReadInConfig()
+
+	cmd.ref = viper.GetString("reference")
 }
 
 func (cmd *cmdProfile) Run(args []string) {
@@ -30,5 +44,5 @@ func (cmd *cmdProfile) Run(args []string) {
 		log.Fatalf("Can not find strain information for %s from species_map.json\n", *cmd.prefix)
 	}
 
-	meta.GenomePosProfiling(strains, *cmd.ref)
+	meta.GenomePosProfiling(strains, cmd.ref)
 }
